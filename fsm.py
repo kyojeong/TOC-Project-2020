9fbe5ac4bb3689d836cad2ebb720747347b0276b
kyojeong <t3839620@gmail.com>
2019-12-05 14:58:44 +0800

    finish

--- fsm.py
+++ fsm.py
@@ -2,7 +2,6 @@ from transitions.extensions import GraphMachine
 
 from utils import send_text_message,send_image_url,movie
 
-
 class TocMachine(GraphMachine):
     def __init__(self, **machine_configs):
         self.machine = GraphMachine(model=self, **machine_configs)
--- utils.py
+++ utils.py
@@ -1,5 +1,4 @@
 import os
-
 from linebot import LineBotApi, WebhookParser
 from linebot.models import MessageEvent, TextMessage, TextSendMessage,ImageSendMessage
 
