6dd9631d366878ed70935ba3afc595054e911c74
kyojeong <t3839620@gmail.com>
2019-12-11 14:58:04 +0800

    final

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ dotenv_path = os.path.join(os.path.dirname(__file__), 'example.env')
 load_dotenv(dotenv_path)
 
 machine = TocMachine(
-    states=["user", "hello","a","b","c","movie"
+    states=["user", "hello","movie"
             ,"q1","q2","q3","q4","q5","q6","q7","q8","q9","q10","q11","q12","q13","q14"
             ,"q15","q16","q17","q18","q19","q20","q21","q22"
             ,"a1","a2","a3","a4","a5","a6","a7","a8","a9","a10","a11","a12","a13","a14","a15"
