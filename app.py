4cd2aaf6fc9247351ad4b983dcd9718e9260f5df
kyojeong <t3839620@gmail.com>
2019-12-11 14:50:09 +0800

    final

--- app.py
+++ app.py
@@ -8,13 +8,14 @@ from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 from fsm import TocMachine
 from utils import send_text_message
+import pygraphviz
+
 
 dotenv_path = os.path.join(os.path.dirname(__file__), 'example.env')
 load_dotenv(dotenv_path)
 
 machine = TocMachine(
-    states=["user", "hello","movie","t1","t2","t3","t4","t5","t6","t7","t8"
-            ,"t9","t10","t11","t12","t13","t14","t15","t16","t17","t18","t19","t20","t21","t22"
+    states=["user", "hello","a","b","c","movie"
             ,"q1","q2","q3","q4","q5","q6","q7","q8","q9","q10","q11","q12","q13","q14"
             ,"q15","q16","q17","q18","q19","q20","q21","q22"
             ,"a1","a2","a3","a4","a5","a6","a7","a8","a9","a10","a11","a12","a13","a14","a15"
@@ -23,101 +24,84 @@ machine = TocMachine(
         {"trigger": "advance","source": "user","dest": "hello","conditions": "is_going_to_hello"},        
         {"trigger": "go_back", "source":"hello", "dest": "user"},
         
+        {"trigger": "advance","source": "user","dest": "a","conditions": "is_going_to_a"}, 
+        {"trigger": "advance","source": "a","dest": "b","conditions": "is_going_to_b"},
+        {"trigger": "advance","source": "b","dest": "c","conditions": "is_going_to_c"},
+        {"trigger": "go_back", "source":"c", "dest": "user"},
+        
+        
         {"trigger": "advance","source": "user","dest": "movie","conditions": "is_going_to_movie"},        
         {"trigger": "go_back", "source":"movie", "dest": "user"},
         
         
         {"trigger": "advance","source": "user","dest": "q1","conditions": "is_going_to_q1"},        
         
-        {"trigger": "advance","source": "t1","dest": "q2","conditions": "is_going_to_q2"},              
-        {"trigger": "advance","source": "t1","dest": "q3","conditions": "is_going_to_q3"},        
+        {"trigger": "advance","source": "q1","dest": "q2","conditions": "is_going_to_q2"},              
+        {"trigger": "advance","source": "q1","dest": "q3","conditions": "is_going_to_q3"},        
         
-        {"trigger": "advance","source": "t2","dest": "q4","conditions": "is_going_to_q4"},        
-        {"trigger": "advance","source": "t2","dest": "q5","conditions": "is_going_to_q5"},  
+        {"trigger": "advance","source": "q2","dest": "q4","conditions": "is_going_to_q4"},        
+        {"trigger": "advance","source": "q2","dest": "q5","conditions": "is_going_to_q5"},  
         
-        {"trigger": "advance","source": "t3","dest": "q6","conditions": "is_going_to_q6"},        
-        {"trigger": "advance","source": "t3","dest": "q7","conditions": "is_going_to_q7"},
+        {"trigger": "advance","source": "q3","dest": "q6","conditions": "is_going_to_q6"},        
+        {"trigger": "advance","source": "q3","dest": "q7","conditions": "is_going_to_q7"},
         
-        {"trigger": "advance","source": "t4","dest": "q8","conditions": "is_going_to_q8"},        
-        {"trigger": "advance","source": "t4","dest": "q9","conditions": "is_going_to_q9"},
+        {"trigger": "advance","source": "q4","dest": "q8","conditions": "is_going_to_q8"},        
+        {"trigger": "advance","source": "q4","dest": "q9","conditions": "is_going_to_q9"},
         
-        {"trigger": "advance","source": "t5","dest": "q10","conditions": "is_going_to_q10"},        
-        {"trigger": "advance","source": "t5","dest": "q11","conditions": "is_going_to_q11"},
+        {"trigger": "advance","source": "q5","dest": "q10","conditions": "is_going_to_q10"},        
+        {"trigger": "advance","source": "q5","dest": "q11","conditions": "is_going_to_q11"},
         
-        {"trigger": "advance","source": "t6","dest": "q12","conditions": "is_going_to_q12"},        
-        {"trigger": "advance","source": "t6","dest": "q11","conditions": "is_going_to_q11"}, 
+        {"trigger": "advance","source": "q6","dest": "q12","conditions": "is_going_to_q12"},        
+        {"trigger": "advance","source": "q6","dest": "q11","conditions": "is_going_to_q11"}, 
         
-        {"trigger": "advance","source": "t7","dest": "q11","conditions": "is_going_to_q11"},        
-        {"trigger": "advance","source": "t7","dest": "q10","conditions": "is_going_to_q10"}, 
+        {"trigger": "advance","source": "q7","dest": "q11","conditions": "is_going_to_q11"},        
+        {"trigger": "advance","source": "q7","dest": "q10","conditions": "is_going_to_q10"}, 
         
-        {"trigger": "advance","source": "t8","dest": "q13","conditions": "is_going_to_q13"},        
-        {"trigger": "advance","source": "t8","dest": "q14","conditions": "is_going_to_q14"},
+        {"trigger": "advance","source": "q8","dest": "q13","conditions": "is_going_to_q13"},        
+        {"trigger": "advance","source": "q8","dest": "q14","conditions": "is_going_to_q14"},
         
-        {"trigger": "advance","source": "t9","dest": "q15","conditions": "is_going_to_q15"},        
-        {"trigger": "advance","source": "t9","dest": "q16","conditions": "is_going_to_q16"},
+        {"trigger": "advance","source": "q9","dest": "q15","conditions": "is_going_to_q15"},        
+        {"trigger": "advance","source": "q9","dest": "q16","conditions": "is_going_to_q16"},
         
-        {"trigger": "advance","source": "t10","dest": "q17","conditions": "is_going_to_q17"},        
-        {"trigger": "advance","source": "t10","dest": "q18","conditions": "is_going_to_q18"},
+        {"trigger": "advance","source": "q10","dest": "q17","conditions": "is_going_to_q17"},        
+        {"trigger": "advance","source": "q10","dest": "q18","conditions": "is_going_to_q18"},
         
-        {"trigger": "advance","source": "t11","dest": "q19","conditions": "is_going_to_q19"},        
-        {"trigger": "advance","source": "t11","dest": "q20","conditions": "is_going_to_q20"},
+        {"trigger": "advance","source": "q11","dest": "q19","conditions": "is_going_to_q19"},        
+        {"trigger": "advance","source": "q11","dest": "q20","conditions": "is_going_to_q20"},
         
-        {"trigger": "advance","source": "t12","dest": "q21","conditions": "is_going_to_q21"},        
-        {"trigger": "advance","source": "t12","dest": "q22","conditions": "is_going_to_q22"},
+        {"trigger": "advance","source": "q12","dest": "q21","conditions": "is_going_to_q21"},        
+        {"trigger": "advance","source": "q12","dest": "q22","conditions": "is_going_to_q22"},
         
-        {"trigger": "advance","source": "t13","dest": "a15","conditions": "is_going_to_a15"},        
-        {"trigger": "advance","source": "t13","dest": "a10","conditions": "is_going_to_a10"}, 
+        {"trigger": "advance","source": "q13","dest": "a15","conditions": "is_going_to_a15"},        
+        {"trigger": "advance","source": "q13","dest": "a10","conditions": "is_going_to_a10"}, 
         
-        {"trigger": "advance","source": "t14","dest": "a12","conditions": "is_going_to_a12"},        
-        {"trigger": "advance","source": "t14","dest": "a15","conditions": "is_going_to_a15"},
+        {"trigger": "advance","source": "q14","dest": "a12","conditions": "is_going_to_a12"},        
+        {"trigger": "advance","source": "q14","dest": "a15","conditions": "is_going_to_a15"},
         
-        {"trigger": "advance","source": "t15","dest": "a7","conditions": "is_going_to_a7"},        
-        {"trigger": "advance","source": "t15","dest": "a4","conditions": "is_going_to_a4"}, 
+        {"trigger": "advance","source": "q15","dest": "a7","conditions": "is_going_to_a7"},        
+        {"trigger": "advance","source": "q15","dest": "a4","conditions": "is_going_to_a4"}, 
         
-        {"trigger": "advance","source": "t16","dest": "a1","conditions": "is_going_to_a1"},        
-        {"trigger": "advance","source": "t16","dest": "a2","conditions": "is_going_to_a2"},
+        {"trigger": "advance","source": "q16","dest": "a1","conditions": "is_going_to_a1"},        
+        {"trigger": "advance","source": "q16","dest": "a2","conditions": "is_going_to_a2"},
         
-        {"trigger": "advance","source": "t17","dest": "a9","conditions": "is_going_to_a9"},        
-        {"trigger": "advance","source": "t17","dest": "a1","conditions": "is_going_to_a1"},
+        {"trigger": "advance","source": "q17","dest": "a9","conditions": "is_going_to_a9"},        
+        {"trigger": "advance","source": "q17","dest": "a1","conditions": "is_going_to_a1"},
         
-        {"trigger": "advance","source": "t18","dest": "a6","conditions": "is_going_to_a6"},        
-        {"trigger": "advance","source": "t18","dest": "a10","conditions": "is_going_to_a10"}, 
+        {"trigger": "advance","source": "q18","dest": "a6","conditions": "is_going_to_a6"},        
+        {"trigger": "advance","source": "q18","dest": "a10","conditions": "is_going_to_a10"}, 
         
-        {"trigger": "advance","source": "t19","dest": "a3","conditions": "is_going_to_a3"},        
-        {"trigger": "advance","source": "t19","dest": "a5","conditions": "is_going_to_a5"}, 
+        {"trigger": "advance","source": "q19","dest": "a3","conditions": "is_going_to_a3"},        
+        {"trigger": "advance","source": "q19","dest": "a5","conditions": "is_going_to_a5"}, 
         
-        {"trigger": "advance","source": "t20","dest": "a14","conditions": "is_going_to_a14"},        
-        {"trigger": "advance","source": "t20","dest": "a9","conditions": "is_going_to_a9"},
+        {"trigger": "advance","source": "q20","dest": "a14","conditions": "is_going_to_a14"},        
+        {"trigger": "advance","source": "q20","dest": "a9","conditions": "is_going_to_a9"},
         
-        {"trigger": "advance","source": "t21","dest": "a8","conditions": "is_going_to_a8"},        
-        {"trigger": "advance","source": "t21","dest": "a7","conditions": "is_going_to_a7"},
+        {"trigger": "advance","source": "q21","dest": "a8","conditions": "is_going_to_a8"},        
+        {"trigger": "advance","source": "q21","dest": "a7","conditions": "is_going_to_a7"},
         
-        {"trigger": "advance","source": "t22","dest": "a11","conditions": "is_going_to_a11"},        
-        {"trigger": "advance","source": "t22","dest": "a13","conditions": "is_going_to_a13"}, 
+        {"trigger": "advance","source": "q22","dest": "a11","conditions": "is_going_to_a11"},        
+        {"trigger": "advance","source": "q22","dest": "a13","conditions": "is_going_to_a13"}, 
         
-        
-        {"trigger": "go_back", "source":"q1", "dest": "t1"}, 
-        {"trigger": "go_back", "source":"q2", "dest": "t2"},
-        {"trigger": "go_back", "source":"q3", "dest": "t3"},
-        {"trigger": "go_back", "source":"q4", "dest": "t4"},
-        {"trigger": "go_back", "source":"q5", "dest": "t5"},
-        {"trigger": "go_back", "source":"q6", "dest": "t6"},
-        {"trigger": "go_back", "source":"q7", "dest": "t7"},
-        {"trigger": "go_back", "source":"q8", "dest": "t8"},
-        {"trigger": "go_back", "source":"q9", "dest": "t9"},
-        {"trigger": "go_back", "source":"q10", "dest": "t10"},
-        {"trigger": "go_back", "source":"q11", "dest": "t11"},
-        {"trigger": "go_back", "source":"q12", "dest": "t12"},
-        {"trigger": "go_back", "source":"q13", "dest": "t13"},
-        {"trigger": "go_back", "source":"q14", "dest": "t14"},
-        {"trigger": "go_back", "source":"q15", "dest": "t15"},
-        {"trigger": "go_back", "source":"q16", "dest": "t16"},
-        {"trigger": "go_back", "source":"q17", "dest": "t17"},
-        {"trigger": "go_back", "source":"q18", "dest": "t18"},
-        {"trigger": "go_back", "source":"q19", "dest": "t19"},
-        {"trigger": "go_back", "source":"q20", "dest": "t20"},
-        {"trigger": "go_back", "source":"q21", "dest": "t21"},
-        {"trigger": "go_back", "source":"q22", "dest": "t22"},
         
         {"trigger": "go_back", "source":"a1", "dest": "user"},
         {"trigger": "go_back", "source":"a2", "dest": "user"},
@@ -222,6 +206,7 @@ def webhook_handler():
 @app.route("/show-fsm", methods=["GET"])
 def show_fsm():
     machine.get_graph().draw("fsm.png", prog="dot", format="png")
+    
     return send_file("fsm.png", mimetype="image/png")
 
 
